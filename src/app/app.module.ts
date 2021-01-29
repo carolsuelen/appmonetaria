da01b86e3e58f0df0015edee816234a7004dc128
nataliasouza <nataliasouza.ti@hotmail.com>
2021-01-29 19:23:44 -0300

    atualização do arquivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { footerComponent } from './footer/footer.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    footerComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
