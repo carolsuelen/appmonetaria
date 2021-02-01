71d1d91964e2fac4dadfc2af5f2747b1e1338224
nataliasouza <nataliasouza.ti@hotmail.com>
2021-02-01 04:29:35 -0300

    Atualização dos componentes e css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { CadastroComponent } from './cadastro/cadastro.component';
 import { ExtratoComponent } from './extrato/extrato.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -24,7 +25,7 @@ const routes: Routes = [ {
   component: PainelUsuarioComponent,
 },{
   path: 'cadastro',
-  component: LoginComponent,
+  component: CadastroComponent,
 }];
 
 @NgModule({
