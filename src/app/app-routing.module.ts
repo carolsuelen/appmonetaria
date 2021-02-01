09b9a824373bad1da7a6158e7857bf0788694520
nataliasouza <nataliasouza.ti@hotmail.com>
2021-02-01 02:16:05 -0300

    Atualização dos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { CadastroComponent } from './cadastro/cadastro.component';
 import { ExtratoComponent } from './extrato/extrato.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { PainelUsuarioComponent } from './painel-usuario/painel-usuario.component';
 
 
 const routes: Routes = [ {
@@ -14,14 +14,17 @@ const routes: Routes = [ {
   path: 'home',
   component: HomeComponent,
 },{
-  path: 'extrato',
-  component: ExtratoComponent,
-},{
   path: 'login',
   component: LoginComponent,
 },{
+  path: 'extrato',
+  component: ExtratoComponent,
+},{
+  path: 'painel-usuario',
+  component: PainelUsuarioComponent,
+},{
   path: 'cadastro',
-  component: CadastroComponent,
+  component: LoginComponent,
 }];
 
 @NgModule({
